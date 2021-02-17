3211b708a7887c779767182950382a8ff93790df
Hong La <hongla@kms-technology.com>
2021-02-17 15:12:14 +0700

    Add target to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
-module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-})
+module.exports = {
+  target: 'serverless',
+  withMDX: withMDX({
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  }),
+}
