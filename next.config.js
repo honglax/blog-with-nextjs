3ca42f391450c200e7dc6689e973bcffb9c52bb3
Hong La <hongla@kms-technology.com>
2021-02-17 15:54:49 +0700

    Deploy with Netlify

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 module.exports = withMDX({
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  target: 'serverless',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
